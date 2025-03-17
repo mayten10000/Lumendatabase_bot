a4af0edd0f18429514b3b82136a550feafdf0b91
mayten10000 <106926459+mayten10000@users.noreply.github.com>
2025-03-17 23:50:43 +0300

    reply added

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.types import Message
 import asyncio
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+import request
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 
 API_TOKEN = '7897800053:AAG0gOh2OGfsa1Cd_sV-x3fFAMjtw-qs6N8'
 
@@ -10,7 +17,25 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def send_welcome(message: Message):
-    await message.reply("Привет! Я бот для работы с Lumendatabase.")
+    await message.reply("Привет! Я бот для работы с Lumendatabase. Прожми /check_notifications")
+
+@dp.message(Command("check_notifications"))
+async def check_notifications(message: Message):
+    await message.reply("Проврка")
+
+    notifications = await fetch_api_lumendatabase
+
+    if notifications:
+        for notice in notifications:
+            await message.answer(
+                f"Уведомление: {notice.get('title', 'Без названия')}\nСсылка: {notice.get('url', 'Нет ссылки')}")
+
+    else:
+        await message.reply("Уведомлений нет")
+
+    await message.reply("Успешно")
+
+
 
 async def main():
     await dp.start_polling(bot)
