2578e70e02dab982b5db73bfd655ac5db761fe8a
John9xDoe <100959916+John9xDoe@users.noreply.github.com>
2025-03-24 13:09:00 +0300

    Добавление парсера в виде отдельного скрипта
    Добавлен парсер в виде отдельного скрипта (по сравнению с предыдущим парсером добавлен переход по сайтам, которые хранятся в sites.csv и добавлена запись уведомлений в sqlite3 - вероятно временное решение)

---
+++ parser.py
@@ -0,0 +1,95 @@
+import logging
+import aiohttp
+from bs4 import BeautifulSoup
+import asyncio
+from aiocache import cached
+import sqlite3
+import csv
+
+###########
+
+logging.basicConfig(level=logging.INFO)
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
+@cached(ttl=60)
+async def parser_lumen():
+    async with aiohttp.ClientSession(headers=headers) as session:
+        try:
+            base_url = "https://lumendatabase.org/notices/"
+
+            with open('sites.csv', newline='', encoding='utf-8') as csvfile:
+                reader = csv.reader(csvfile)  
+                sites = [row[0] for row in reader]  
+            
+            notices = []  
+
+            for site_url in sites:
+
+                p = 1
+                
+                while True:
+                                        
+                    page_url = f'{base_url}search?page={p}&term={site_url}'
+                    
+                    print(f"Парсинг {p}-ой страницы: {page_url}")
+
+                    async with session.get(page_url) as response:
+                        if response.status == 200:
+                            html = await response.text()
+                            soup = BeautifulSoup(html, 'html.parser')
+
+                            for item in soup.select('li.notice.result'):  
+                                title = item.select_one('h3.title a').text.strip() if item.select_one('.title') else "Без названия"
+                                url = item.select_one('a')['href'] if item.select_one('a') else "#"
+                                notices.append({
+                                    "title": title,
+                                    "url": url.replace('/notices/', ''),
+                                    "site": site_url
+                                })
+                            await asyncio.sleep(2)
+                        else:
+                            logging.error(f"Ошибка при запросе страницы {p}: {response.status}")
+                            break
+                    
+                    next_button = soup.select_one('span.next a[rel="next"]')
+
+                    if not next_button:
+                        print(f"Уведомления с сайта {site_url} успешно собраны")
+                        break
+
+                    p += 1
+
+                    if p == 11: break # DEL (добавить авторизацию Lumendatabase для просмотра дальше 10-ой страницы)
+
+            return notices  
+
+        except Exception as e:
+            logging.error(f"Ошибка при подключении к сайту: {e}")
+            return []
+
+async def write_notices():
+
+    notifications = await parser_lumen()
+
+    conn = sqlite3.connect("notifications.db")
+    cursor = conn.cursor()
+    
+    if notifications:
+        
+        for notice in notifications:
+
+                site_notice = notice.get('site', 'Без сайта')
+                title_notice = notice.get('title', 'Без названия')
+                id_notice = notice.get('url', 'Нет идентификатора')
+
+                
+                #text_notice = f"{notice.get('title', 'Без названия')}\nСсылка на уведомление: {notice.get('url', 'Нет ссылки')}\nСсылка на сайт: {notice.get('site', 'Без сайта')}"
+                
+                cursor.execute("INSERT INTO notifications (site, notice_id, title) VALUES (?, ?, ?)", (site_notice, id_notice, title_notice))
+                
+    conn.commit()
+    conn.close()
+
+    logging.info('Все уведомления успешно собраны !')
+
+asyncio.run(write_notices())
