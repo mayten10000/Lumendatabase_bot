cdb32dc1df62b36f1169319308c43a71040f691a
John9xDoe <100959916+John9xDoe@users.noreply.github.com>
2025-03-25 21:22:24 +0300

    Добавление в запуск остальных скриптов*
    * 2/4 скриптов пока "не встроены в общий процесс" (т.е не могут работать вместе)

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import schedule
 import time
 
 import parser
+import ai_handler # пока не встроен в общий процесс
+import filler_g_forms # пока не встроен в общий процесс
 import tg_bot
 
 import logging
@@ -17,8 +19,8 @@ from urllib.parse import urlencode, urlparse, parse_qs, urlunparse
 
 def daily_check():
     parser()
-    # прогон уведомлений через AI и запись ответов
-    # заполнение гугл форм из ответов
+    # ai_handler() # прогон уведомлений через AI 
+    # filler_g_forms() # заполнение гугл форм из ответов
     tg_bot() # вывод уведомлений (+-) и ответов (-) в тг-боте
     
 schedule.every().day.at("14:15").do(daily_check)
